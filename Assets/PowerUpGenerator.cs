0027370030c161474edadba6f214cf250e61058d
Kendal Mitchell <klm475@drexel.edu>
2022-03-17 21:37:50 -0400

    Fruit Prefabs, Color Map Prefab Instantiation, Scriptable Objects
    Added prefabs for fruits, a color map instantiation version of fruit spawning. Also changed fruit objects to utilize a scriptable object.

--- Assets/PowerUpGenerator.cs
+++ Assets/PowerUpGenerator.cs
@@ -2,14 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[System.Serializable]
+public class colorToPrefab
+{
+    public GameObject[] prefab;
+    public Color color;
+}
+
 public class PowerUpGenerator : MonoBehaviour
 {
     public static PowerUpGenerator instance;
     public GameObject[] powerUp;
 
+    public bool mapUsed = false;
+    public Texture2D foodMap;
 
-    public float coundownspee;
+    public colorToPrefab[] colortoPrefab;
+    public GameObject parentObj;
+
+    public float countdownSpeed;
     public float MaxSpeed;
+
 
     private void Awake()
     {
@@ -24,19 +38,61 @@ public class PowerUpGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        coundownspee -= Time.deltaTime;
-        if (coundownspee <= 0)
+        countdownSpeed -= Time.deltaTime;
+        if (countdownSpeed <= 0)
         {
-            GeneratePowerup();
-            coundownspee = MaxSpeed;
+            int randNo = Random.Range(0, 6);
+            Debug.Log("random works" + randNo);
+
+            if (randNo == 0) // 0 is the bottle
+            {
+                GeneratePowerup(randNo);
+            }
+
+            else if (mapUsed)
+            {
+                GenerateMap(randNo);
+                mapUsed = false;
+            }
+            
+            else
+            {
+                GeneratePowerup(randNo);
+                mapUsed = true;
+            }
+            
+            countdownSpeed = MaxSpeed;
         }
-        Debug.Log(coundownspee);
+        Debug.Log(countdownSpeed);
     }
-    public void GeneratePowerup()
+    public void GeneratePowerup(int randNo)
     {
-        int Randmo = Random.Range(0, 2);
-        Debug.Log("random works" + Randmo);
-        Instantiate(powerUp[Randmo], transform.position, transform.rotation);
+        Instantiate(powerUp[randNo], transform.position, transform.rotation);
+    }
+ 
+    void GenerateMap(int randNo)
+    {
+        for (int x = 0; x < foodMap.width; x++)
+        {
+            for (int y = 0; y < foodMap.height; y++)
+            {
+                GenerateFoods(x, y, randNo);
+            }
+        }
     }
 
+    void GenerateFoods(int x, int y, int food)
+    {
+        Color mapColor = foodMap.GetPixel(x, y);
+        
+        foreach (colorToPrefab obj in colortoPrefab)
+        {
+            if (obj.color.Equals(mapColor))
+            {
+                Vector2 pos = new Vector2(x, y - 3);
+                Instantiate(obj.prefab[food], pos, Quaternion.identity, parentObj.transform);
+            }
+        }
+    }
+    
 }
